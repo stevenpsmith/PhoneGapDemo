9e6cb75782a599514c5f6371063340d8a2297908
stevenpsmith <ssmith@chariotsolutions.com>
2011-02-11 16:14:03 -0500

    android contact save working

--- core_source/javascript/views/SpeakerDetails.js
+++ core_source/javascript/views/SpeakerDetails.js
@@ -10,6 +10,7 @@ Demo.views.SpeakerDetails = Ext.extend(Ext.Panel, {
 		this.detailView = new Ext.Component({
 			tpl: new Ext.XTemplate(
 			    	'<div class="data-set">',
+					  '<input id="speakerId" type="hidden" value="{id}" />',
 				      '<div class="field"><span class="label">Name:</span> {name}</div>',
 					  '<div class="field"><span class="label">Position:</span> {position:ellipsis(40, true)}</div>',
 					  '<div class="field"><span class="label">Bio:</span> {bio:ellipsis(60, true)}</div>',
@@ -20,7 +21,8 @@ Demo.views.SpeakerDetails = Ext.extend(Ext.Panel, {
 		
 		this.button = new Ext.Button({
 			text: 'Add to Address Book',
-			width: 200
+			width: 200,
+            handler: this.addContact
 		});
 		
 		var viewItems = [this.detailView];
@@ -47,5 +49,32 @@ Demo.views.SpeakerDetails = Ext.extend(Ext.Panel, {
 	onPanelActivate: function() {
 		Ext.getCmp('navBar').items.get(0).show();
 		Ext.getCmp('navBar').items.get(2).hide();
-	}
+	},
+
+    addContact: function() {
+        alert('getting ready to save contact');
+       sample_contact = { "givenName": "Steve", "familyName" : "Smith"};
+//       navigator.contacts.newContact(sample_contact);
+		var speakerId = Ext.getDom('speakerId').value;
+		var store = Ext.getStore('SpeakerStore');
+		var speaker = store.getById(parseInt(speakerId, 10));
+       	
+        var contactName = new ContactName();
+	   	contactName.givenName = speaker.get('first_name');
+	   	contactName.familyName = speaker.get('last_name');
+        
+	   	var newContact = navigator.service.contacts.create();
+	   	newContact.name = contactName;
+        newContact.displayName = speaker.get('name');
+        newContact.note = speaker.get('position');
+       	
+       	newContact.save(
+       			function(contacts) {
+       				alert('Contact Saved');
+       			},
+       			function(contactError) {
+       				alert('Error saving contact: ' + contactError.code);
+       			}
+       	);
+    }
 });
